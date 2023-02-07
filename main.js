7282db459137269fa723a5336165d33ef0de142d
GyunHee <ef3232@naver.com>
2023-02-07 11:51:00 +0900

    carrot 이미지 추가

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+'use strict';
+
 const horozontal = document.querySelector('.horozontal');
 const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const body = document.querySelector('body');
+
+const bodyRect = body.getBoundingClientRect();
+
+const CARROT_COUNT = 10;
+
+function initGame() {
+    addCarrot(CARROT_COUNT);
+}
+
+function addCarrot(count) {
+    for (let i = 0; i < count; i++) {
+        const carrot = document.createElement('img');
+        carrot.setAttribute('src', './img/carrot.png');
+        carrot.setAttribute('class', 'carrot');
+        body.appendChild(carrot);
+    }
+}
 
 document.addEventListener('mousemove', (event) => {
     const x = event.clientX;
@@ -15,3 +35,5 @@ document.addEventListener('mousemove', (event) => {
     tag.style.transform = `translate(${x+20}px, ${y+20}px)`;
     tag.innerHTML = `${x}, ${y}`;
 });
+
+initGame();
